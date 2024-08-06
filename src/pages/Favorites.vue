f99e4de86ab82b4920260f642425e3afa5a2e155
HM <merzhoev04.04@mail.ru>
2024-08-06 22:58:43 +0300

    profile favorites

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -3,8 +3,10 @@ import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
 import CardList from '../components/CardList.vue'
+import infoBlock from '../components/infoBlock.vue'
 
 const favorites = ref([])
+const isFavorite = ref(true)
 
 onMounted(async () => {
   try {
@@ -12,6 +14,9 @@ onMounted(async () => {
       'https://3b07d63039234082.mokky.dev/favorites?_relations=items'
     )
     favorites.value = data.map((obj) => obj.item)
+    if (favorites.value.length == 0) {
+      isFavorite.value = false
+    }
   } catch (err) {
     console.log(err)
   }
@@ -21,5 +26,42 @@ onMounted(async () => {
 <template>
   <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
 
-  <CardList :items="favorites" isFavorites />
+  <div class="min-h-96">
+    <CardList v-if="isFavorite" :items="favorites" isFavorites />
+    <div class="flex flex-col items-center gap-8" v-if="!isFavorite">
+      <infoBlock
+        imageUrl="/emoji-1.png"
+        title="Закладок нет :("
+        description="Вы ничего не добавляли в закладки"
+      />
+      <RouterLink to="/">
+        <button class="bg-lime-400 text-white py-4 px-14 rounded-2xl flex items-center gap-5">
+          <svg
+            class="rotate-180"
+            width="16"
+            height="14"
+            viewBox="0 0 16 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 7H14.7143"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M8.71436 1L14.7144 7L8.71436 13"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p>Вернуться назад</p>
+        </button>
+      </RouterLink>
+    </div>
+  </div>
 </template>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,14 +3,18 @@ import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
 import CardList from '../components/CardList.vue'
+import infoBlock from '../components/infoBlock.vue'
 
 const orders = ref([])
+const isOrder = ref(true)
 
 onMounted(async () => {
   try {
     const { data } = await axios.get('https://3b07d63039234082.mokky.dev/orders')
     orders.value = data.flatMap((item) => item.items)
-    console.log(orders.value)
+    if (orders.value.length == 0) {
+      isOrder.value = false
+    }
   } catch (err) {
     console.log(err)
   }
@@ -18,7 +22,44 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h2 class="text-3xl font-bold mb-8">Мои покупки</h2>
+  <h2 class="text-3xl font-bold mb-8">Мои заказы</h2>
 
-  <CardList :items="orders" isFavorites />
+  <div class="min-h-96">
+    <CardList v-if="isOrder" :items="orders" isFavorites />
+    <div class="flex flex-col items-center gap-8" v-if="!isOrder">
+      <infoBlock
+        imageUrl="/emoji-2.png"
+        title="У вас нет заказов"
+        description="Вы нищеброд? Оформите хотя бы один заказ."
+      />
+      <RouterLink to="/">
+        <button class="bg-lime-400 text-white py-4 px-14 rounded-2xl flex items-center gap-5">
+          <svg
+            class="rotate-180"
+            width="16"
+            height="14"
+            viewBox="0 0 16 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 7H14.7143"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M8.71436 1L14.7144 7L8.71436 13"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p>Вернуться назад</p>
+        </button>
+      </RouterLink>
+    </div>
+  </div>
 </template>
